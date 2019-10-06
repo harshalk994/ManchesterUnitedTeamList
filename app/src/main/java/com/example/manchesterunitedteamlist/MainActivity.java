77649002a5120882601fd282de7b2e082e390ad5
Harshal <harshalkasle1994@gmail.com>
2019-10-06 20:13:34 +0100

    version 1.2

--- app/src/main/java/com/example/manchesterunitedteamlist/MainActivity.java
+++ app/src/main/java/com/example/manchesterunitedteamlist/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.manchesterunitedteamlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playerButton = null;
+    //private Button staffButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //wire the elements
+        playerButton = findViewById(R.id.button);
+        //staffButton = findViewById(R.id.button2);
+
+        //deal with button
+        playerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //make an intent to go to the playerlist activity
+                //Intent intent = new Intent(MainActivity.this, PlayerListActivity.class);
+                //startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/manchesterunitedteamlist/PlayerListActivity.java
@@ -0,0 +1,17 @@
+package com.example.manchesterunitedteamlist;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toast.makeText(getApplicationContext(),"Opened 2nd activity")
+    }
+}
